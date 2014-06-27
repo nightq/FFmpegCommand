fa394070417339fc6126cae9001875d124373a3d
nightq <472974979@qq.com>
2014-06-27 10:16:30 +0800

    ffmpeg可以运行，但是重复执行会崩溃

---
+++ jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := ffmpeg
+LOCAL_SRC_FILES := ffmpeg.c cmdutils.c ffmpeg_opt.c ffmpeg_filter.c  
+LOCAL_LDLIBS := -llog -ljnigraphics -lz 
+LOCAL_C_INCLUDES := config.h ffmpeg.h cmdutils.h cmdutils_common_opts.h libavutil/*.h libavformat/*.h compat/*.h libavresample/*.h libpostproc/*.h libavcodec/*.h libavdevice/*.h libavfilter/*.h libavswscale/*.h
+LOCAL_SHARED_LIBRARIES := libavformat libavcodec libswscale libavutil libswresample libavfilter libavdevice
+include $(BUILD_SHARED_LIBRARY)
+$(call import-module,ffmpeg/android/arm)
